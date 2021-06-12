b90acc07e863fa833bd9665d176e48964cd883ea
grbrooks <grahambrooks@hotmail.co.uk>
2021-06-12 10:04:53 +0100

    first commit

---
+++ profile.php
@@ -0,0 +1,17 @@
+
+<html>
+    <head>
+
+    </head>
+    <body>
+    <?php
+function myTest(){
+    $x=5;
+    echo "<p> Variable x insiide function is: $x</p>";
+}
+myTest();
+echo"<p>Variable x outside function is: $x</p>";
+?>
+        
+    </body>
+</html>
